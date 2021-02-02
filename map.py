eb9c03176b197aa5cf7b67701ca3e7b28e93c670
rus_is <kniggaaaa@mail.ru>
2021-02-02 16:24:44 +0300

    My Map(return image without telethon)

--- map.py
+++ map.py
@@ -3,20 +3,21 @@ from io import BytesIO
 
 import requests
 from PIL import Image
-from telethon.client import buttons
+
 
 
 class Map:
-    buttons = {'left': 'self.x -= 10',
-               'right': 'self.x += 10',
-               'up': 'self.y += 10',
-               'down': 'self.y -= 10'
-               }
+
 
     def __init__(self, x, y, scale):
         self.pos_x = x
         self.pos_y = y
         self.scale = scale
+        self.buttons = {'left': 'self.x -= 10',
+               'right': 'self.x += 10',
+               'up': 'self.y += 10',
+               'down': 'self.y -= 10'
+               }
 
     def get_map(self, x, y, scale):
         api_server = "http://static-maps.yandex.ru/1.x/"
@@ -34,50 +35,5 @@ class Map:
         return Image.open(BytesIO(response.content))
 
     def shift(self, button):
-        eval(buttons[button])
+        eval(self.buttons[button])
         return self.get_map()
-
-
-=======
-from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-
-import requests
-from PIL import Image
-from telethon.client import buttons
-
-
-class Map:
-    buttons = {'left': 'self.x -= 10',
-               'right': 'self.x += 10',
-               'up': 'self.y += 10',
-               'down': 'self.y -= 10'
-               }
-
-    def __init__(self, x, y, scale):
-        self.pos_x = x
-        self.pos_y = y
-        self.scale = scale
-
-    def get_map(self, x, y, scale):
-        api_server = "http://static-maps.yandex.ru/1.x/"
-        lon = str(x)
-        lat = str(y)
-        delta = str(scale)
-
-        params = {
-            "ll": ",".join([lon, lat]),
-            "spn": ",".join([delta, delta]),
-            "l": "map"
-        }
-        response = requests.get(api_server, params=params)
-
-        Image.open(BytesIO(
-            response.content)).show()
-
-    def shift(self, button):
-        eval(buttons[button])
-        return self.get_map()
-
-
->>>>>>> origin/master
